1d62aa1d9c31014cb0129b9280506b134cdd0e18
Jornt Verbist <jornt@bist.be>
2025-05-26 17:02:43 +0200

    error

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def get_db():
     finally:
         db.close()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello FastAPI"}
+
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     with open(f"uploaded_files/{file.filename}", "wb") as buffer:
